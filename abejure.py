1d69db48b367b9c87856400664c020cd1d7ad60f
kkidane <73114792+kkidane@users.noreply.github.com>
2020-10-21 11:19:48 -0400

    Add files via upload

---
+++ abejure.py
@@ -0,0 +1,30 @@
+phrase = "abule"
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase[0])
+print(phrase.index("b"))
+print(phrase.replace("u", "o"))
+print(3+4.5)
+x = 3 + 4.5
+print(x)
+from math import*
+print(floor(3.7))
+print(phrase.index("ule"))
+my_num = 5
+print(str(my_num) + " is my favorite number")
+name = input("What is your name: ")
+height = float(input("what is your height in m: "))
+weight = float(input("what is your weight in kgs: "))
+BMI = float(weight/(height*height))
+print(BMI)
+print("your name is " + name +   " and your BMI is "  + str(BMI))
+if BMI > 24:
+    print("overweight")
+if BMI < 15:
+     print("underweight")
+if BMI <= 24 and BMI >= 15:
+    print("prefect weight")
+
+
+
+
